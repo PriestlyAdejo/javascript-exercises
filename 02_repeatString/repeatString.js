089ee4a48cd16282297cf51741708b593bf9ca01
Priestly <priestlyadejo@gmail.com>
2023-06-15 20:44:50 +0100

    Did exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(userInput, repeatFor) {
+    let userString = userInput.toString();
+    let outputString = "";
 
+    if (repeatFor == 0) {
+        return outputString
+    } else if (repeatFor < 0) {
+        return "ERROR"
+    } else if (userInput.length == 0) {
+        return outputString
+    }
+    for (let i=0; i<repeatFor; i++) {
+        outputString += userString
+    }
+    return outputString
 };
 
 // Do not edit below this line
